9f95d8d9f07a8c5cbfd378a92e04f6485b7237fb
ebugaiov <evgeniy.bugaiov@gmail.com>
2024-10-02 20:27:00 +0200

    add group by theme to console

---
+++ import_products.py
@@ -0,0 +1,20 @@
+import csv
+from db import Model, Session, engine
+from models import Product
+
+
+def main():
+    Model.metadata.drop_all(engine)
+    Model.metadata.create_all(engine)
+
+    with Session() as session:
+        with session.begin():
+            with open('products.csv') as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    row['year'] = int(row['year'])
+                    product = Product(**row)
+                    session.add(product)
+
+if __name__ == '__main__':
+    main()
