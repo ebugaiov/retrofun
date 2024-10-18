945fcbd8db43db74c18b78141bdd5a8da9bacf18
ebugaiov <evgeniy.bugaiov@gmail.com>
2024-10-18 15:00:57 +0200

    customers and orders

--- models.py
+++ models.py
@@ -1,7 +1,9 @@
 from typing import Optional
+from datetime import datetime
+from uuid import UUID, uuid4
 from sqlalchemy import String, ForeignKey
 from sqlalchemy import Table, Column
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, mapped_column, relationship, WriteOnlyMapped
 from db import Model
 
 
@@ -62,5 +64,48 @@ class Product(Model):
         secondary=ProductCountry, back_populates='products'
     )
 
+    order_items: WriteOnlyMapped[list['OrderItem']] = relationship(back_populates='product')
+
     def  __repr__(self):
         return f'Product({self.id}, "{self.name}")'
+
+
+class Order(Model):
+    __tablename__ = 'orders'
+
+    id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
+    timestamp: Mapped[datetime] = mapped_column(default=datetime.utcnow, index=True)
+    customer_id: Mapped[UUID] = mapped_column(ForeignKey('customers.id'), index=True)
+
+    customer: Mapped['Customer'] = relationship(back_populates='orders')
+
+    order_items: Mapped[list['OrderItem']] = relationship(back_populates='order')
+
+    def __repr__(self):
+        return f'Order({self.id.hex})'
+
+
+class Customer(Model):
+    __tablename__ = 'customers'
+
+    id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
+    name: Mapped[str] = mapped_column(String(64), index=True, unique=True)
+    address: Mapped[Optional[str]] = mapped_column(String(128))
+    phone: Mapped[Optional[str]] = mapped_column(String(32))
+
+    orders: WriteOnlyMapped['Order'] = relationship(back_populates='customer')
+
+    def __repr__(self):
+        return f'Customer({self.id.hex}, "{self.name}"'
+
+
+class OrderItem(Model):
+    __tablename__ = 'order_items'
+
+    product_id: Mapped[int] = mapped_column(ForeignKey('products.id'), primary_key=True)
+    order_id: Mapped[int] = mapped_column(ForeignKey('orders.id'), primary_key=True)
+    unit_price: Mapped[float]
+    quantity: Mapped[int]
+
+    product: Mapped['Product'] = relationship(back_populates='order_items')
+    order: Mapped['Order'] = relationship(back_populates='order_items')
